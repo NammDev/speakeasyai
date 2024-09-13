34e19d1e3a007abce36e1c6208b517eb1461b5bb
Nam <namdeveloper.ca@gmail.com>
2024-09-13 16:30:42 +0700

    header

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { IBM_Plex_Sans as FontSans } from 'next/font/google'
 
 import './globals.css'
 import { cn } from '@/lib/utils'
+import Header from '@/components/home/header'
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -23,7 +24,8 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
-        {children}
+        <Header />
+        <main>{children}</main>
       </body>
     </html>
   )
